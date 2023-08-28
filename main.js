a9a6fbbb6d73991c63087f545212cf72c122ead1
yasudayo <90016022+yasudayo@users.noreply.github.com>
2023-08-28 15:41:06 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,70 +1,81 @@
 const push = document.getElementById("start1");
 const push1 = document.getElementById("start");
-let readySe = new Audio ("R.mp3");
-let fireSe = new Audio ("F.mp3");
+const TCT = document.getElementById('tc');
+const bb = document.getElementById('back');
+TCT.style.display = 'none';
+bb.style.display = 'none';
+let readySe = new Audio("R.mp3");
+let fireSe = new Audio("F.mp3");
 let fire = false;
 let timer;
 let count = [4, 5, 6, 7, 8, 9, 10, 11, 12];
-let counts = Math.floor(Math.random () * count.length);
-let timer1;
-let timer2;
-let timer3;
-let countup = 0;
-let countup2 = false;
-let countup3 = false;
-function start () {
-    push.style.opacity = 0;
+let counts = Math.floor(Math.random() * count.length);//いつ「FIRE」になるかをランダムに設定
+var startTime;
+var elapsedTime = 0;
+var timerID;
+var timeToadd = 0;
+
+function start() {
+    push.style.display = 'none';
+    TCT.style.display = 'block'
     const word1 = document.getElementById("word1");
     word1.innerText = "READY";
     readySe.play();
-    startTimer ();
+    startTimer();
 }
 document.getElementById("push").style.backgroundColor = 'black';
 
-function startTimer () {
+function startTimer() {
     timer = setInterval(show1, 1000)
 }
 
-function show1 () {
+function show1() {
     count[counts]--;
     if (count[counts] === 0) {
+        clearInterval(timer);
         fire = true;
         console.log(fire);
         word1.innerText = "FIRE";
         fireSe.play();
         countupTimer();
-        count[counts] === 0;
-        countup2 === 0;
-        countup3 === 0;
         console.log(count[counts]);
     }
 }
 
-function countupTimer () {
-    timer1 = setInterval(show2, 1000);
-    timer2 = setInterval(show3, 100);
-    timer3 = setInterval(show4, 10);
+function countupTimer() {
+    startTime = Date.now();
+    countup()
+}
+function countup() {
+    timerID = setTimeout(function () {
+        elapsedTime = Date.now() - startTime + timeToadd;
+        updateTimeText();
+        countup()
+    }, 10);
 }
 
-function show2 () {
-    countup++;
-    console.log(countup);
-}
-function show3 () {
-    countup2++;
-    console.log(countup2);
-}
-function show4 () {
-    (countup3++) / 10;
-    console.log(countup2);
-}
-function p1 () {
-    if (fire === false) {
-        alert("失格");
-        document.location.reload();
-    }else {
-        alert(`${countup}.${countup2}${countup3}秒`);
+function updateTimeText() {
+    var s = Math.floor((elapsedTime % 60000) / 1000);
+    var ms = elapsedTime % 1000;
+    s = ("0" + s).slice(-2);
+    ms = ("00" + ms).slice(-3);
+    TCT.textContent = s + ":" + ms;
+
+    if (s == 60) {
+        clearInterval(timerID);
+        alert("失格")
         document.location.reload();
     }
 }
+function p1() {
+    clearInterval(timerID);
+    bb.style.display = 'block';
 
+    if (fire === false) {
+        alert("失格");
+        document.location.reload();
+    }
+}
+function backb() {
+    document.location.reload();
+}
